361cb1009469b5d6a87cdf94151880a598766254
Cristi Sandu <cristi.sandu@csx.ro>
2023-10-25 01:37:38 +0300

    Connect Firebase to Xcode

--- KaizenX/KaizenX/KaizenXApp.swift
+++ KaizenX/KaizenX/KaizenXApp.swift
@@ -7,9 +7,24 @@
 
 // Punctul de intrare al aplicației
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+      print("Configured Firebase")
+
+    return true
+  }
+}
 
 @main
 struct KaizenXApp: App {
+    
+    // register app delegate for Firebase setup
+      @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContainerView()
