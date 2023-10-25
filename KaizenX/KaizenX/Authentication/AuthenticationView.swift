23ff037c92ad38c3308deba1cf5a984030be5fa2
Cristi Sandu <cristi.sandu@csx.ro>
2023-10-25 03:30:35 +0300

    Add NavigationLink for Sign In

---
+++ KaizenX/KaizenX/Authentication/AuthenticationView.swift
@@ -0,0 +1,36 @@
+//
+//  AuthenticationView.swift
+//  KaizenX
+//
+//  Created by Cristi Sandu on 25.10.2023.
+//
+
+import SwiftUI
+
+struct AuthenticationView: View {
+    var body: some View {
+        VStack {
+            NavigationLink {
+                
+            } label: {
+                Text("Sign in with email")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(height: 44)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .cornerRadius(12)
+            }
+            Spacer() // Împinge NavigationLink-ul în partea de sus a ecranului
+        }
+        .padding()
+        .navigationTitle("Sign in")
+    }
+}
+
+#Preview {
+    NavigationStack {
+        AuthenticationView()
+    }
+    
+}
---
+++ KaizenX/KaizenX/Authentication/SignInEmailView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInEmailView.swift
+//  KaizenX
+//
+//  Created by Cristi Sandu on 25.10.2023.
+//
+
+import SwiftUI
+
+struct SignInEmailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInEmailView()
+}
