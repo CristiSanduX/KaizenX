35cf3c8cf371ddbd9c9f67dd0f0a0b92fc1214c1
Cristi Sandu <cristi.sandu@csx.ro>
2023-10-29 01:19:35 +0300

    Create AuthDataResultModel for cleaner user data handling

--- KaizenX/KaizenX/Authentication/AuthenticationManager.swift
+++ KaizenX/KaizenX/Authentication/AuthenticationManager.swift
@@ -8,11 +8,26 @@
 import Foundation
 import FirebaseAuth
 
+// Pentru a include doar câmpurile relevante din AuthDataResult
+struct AuthDataResultModel {
+    let uid: String
+    let email: String?
+    let photoURL: String?
+    
+    init(user: User) {
+        self.uid = user.uid
+        self.email = user.email
+        self.photoURL = user.photoURL?.absoluteString
+    }
+}
+
 final class AuthenticationManager {
     static let shared = AuthenticationManager()
     private init() { }
     
-    func createUser(email: String, password: String) async throws {
+    func createUser(email: String, password: String) async throws -> AuthDataResultModel {
         let authDataResult = try await Auth.auth().createUser(withEmail: email, password: password)
+        
+        return AuthDataResultModel(user: authDataResult.user)
     }
 }
