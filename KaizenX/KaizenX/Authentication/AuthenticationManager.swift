af5f648225d699fc0a4211a473c11a50617b65f6
Cristi Sandu <cristi.sandu@csx.ro>
2023-10-29 00:42:41 +0300

    Implement createUser async method

--- KaizenX/KaizenX/Authentication/AuthenticationManager.swift
+++ KaizenX/KaizenX/Authentication/AuthenticationManager.swift
@@ -6,9 +6,13 @@
 //
 
 import Foundation
+import FirebaseAuth
 
 final class AuthenticationManager {
     static let shared = AuthenticationManager()
     private init() { }
     
+    func createUser(email: String, password: String) async throws {
+        let authDataResult = try await Auth.auth().createUser(withEmail: email, password: password)
+    }
 }
