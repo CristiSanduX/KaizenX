5c6148e120f5a4fffb9c79eabff360f114d3d89e
Cristi Sandu <cristi.sandu@csx.ro>
2023-11-27 15:13:43 +0200

    Integrate image cropping with UIImagePickerController

--- KaizenX/KaizenX/Core/Profile/PhotoPicker.swift
+++ KaizenX/KaizenX/Core/Profile/PhotoPicker.swift
@@ -1,46 +1,54 @@
-import PhotosUI
+//
+//  PhotoPicker.swift
+//  KaizenX
+//
+//  Created by Cristi Sandu on 27.11.2023.
+//
+
 import SwiftUI
+import UIKit
 
 struct PhotoPicker: UIViewControllerRepresentable {
     @Binding var selectedImage: UIImage?
     var didSelectImage: () -> Void
 
-    func makeUIViewController(context: Context) -> PHPickerViewController {
-        var config = PHPickerConfiguration()
-        config.selectionLimit = 1
-        config.filter = .images
-
-        let picker = PHPickerViewController(configuration: config)
+    func makeUIViewController(context: Context) -> some UIViewController {
+        let picker = UIImagePickerController()
         picker.delegate = context.coordinator
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true // Permite utilizatorilor să editeze imaginea - inclusiv crop.
         return picker
     }
 
-    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {}
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
 
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
 
-    class Coordinator: NSObject, PHPickerViewControllerDelegate {
-        let parent: PhotoPicker
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        var parent: PhotoPicker
 
         init(_ parent: PhotoPicker) {
             self.parent = parent
         }
 
-        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
-            picker.dismiss(animated: true)
-            
-            guard let provider = results.first?.itemProvider, provider.canLoadObject(ofClass: UIImage.self) else { return }
-            
-            provider.loadObject(ofClass: UIImage.self) { image, _ in
-                DispatchQueue.main.async {
-                    if let image = image as? UIImage {
-                        self.parent.selectedImage = image
-                        self.parent.didSelectImage()
-                    }
-                }
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+            if let image = info[.editedImage] as? UIImage {
+                // Aici avem imaginea cropată pe care utilizatorul a selectat-o.
+                parent.selectedImage = image
+                parent.didSelectImage()
+            } else if let image = info[.originalImage] as? UIImage {
+                // Dacă utilizatorul nu a decis să editeze imaginea, folosim originalul.
+                parent.selectedImage = image
+                parent.didSelectImage()
             }
+
+            picker.dismiss(animated: true)
+        }
+
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            picker.dismiss(animated: true)
         }
     }
 }
